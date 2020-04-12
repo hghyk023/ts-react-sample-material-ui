546ecfb11b12030aa87bbfb75a35b2b5a75d7d03
shabe <abeshoma@icloud.com>
2020-04-13 01:20:54 +0900

    サンプルコードを書いてみた https://github.com/mui-org/material-ui/tree/master/docs/src/pages/getting-started/templates/sign-in-side

--- src/MyStyledCopmonent.tsx
+++ src/MyStyledCopmonent.tsx
@@ -1,4 +1,35 @@
-
+import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
+import React from "react";
 
 // スタイルを定義
-const useStyles = make
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            // デフォルトが4px
+            padding: theme.spacing(2)
+        },
+        title: {
+            borderBottom: `2px solid ${theme.palette.primary.main}`
+        }
+    })
+)
+
+// Propsの型を定義
+type Props = {
+    title?: string
+}
+
+// コンポーネントを定義
+const MyStyledComponent: React.FC<Props> = (props: Props) => {
+    // めちゃくちゃわかりやすい!
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <h4 className={classes.title}>
+                {props.title || 'My Styled Component'}
+            </h4>
+        </div>
+    )
+}
+
+export default MyStyledComponent;
